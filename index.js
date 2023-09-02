a92d7b217f9f1baf216109317bf62ad497e1eb20
Arndt Tigges <arndt@tigges.online>
2023-09-02 18:01:41 +0200

    1.1.0 added excel export

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron');
 const path = require('path');
 const Database = require('./src/database.js');
+const fs = require('fs');
+const ExcelJS = require('exceljs');
 
 const database = new Database();
 
@@ -17,32 +19,53 @@ function createWindow() {
   database.sync();
   //win.webContents.openDevTools()
 
-ipcMain.on('saveArticle', async(event, obj) => {
-    try {
-      await database.saveArticle(obj)
-      await loadArticles(win);
-    } catch (error) {
-      console.error('Error saving article:', error);
-    }
-})
+  ipcMain.on('saveArticle', async(event, obj) => {
+      try {
+        await database.saveArticle(obj)
+        await loadArticles(win);
+      } catch (error) {
+        console.error('Error saving article:', error);
+      }
+  })
 
-ipcMain.on('deleteArticle', async(event, id) => {
-    try {
-      await database.deleteArticle(id)
-      await loadArticles(win);
-    } catch (error) {
-      console.error('Error deleting article:', error);
-    }
-})
+  ipcMain.on('deleteArticle', async(event, id) => {
+      try {
+        await database.deleteArticle(id)
+        await loadArticles(win);
+      } catch (error) {
+        console.error('Error deleting article:', error);
+      }
+  })
 
-win.webContents.on('did-finish-load', async() => {
-    try{
-        loadArticles(win);
-    } catch (error) {
-        console.error('Error fetching articles:', error);
-    }
-});
+  win.webContents.on('did-finish-load', async() => {
+      try{
+          loadArticles(win);
+      } catch (error) {
+          console.error('Error fetching articles:', error);
+      }
+  });
 
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Export',
+          click: () => {
+            exportToExcel();
+          },
+        },
+        {
+          label: 'Exit',
+          click: () => {
+            app.quit();
+          },
+        },
+      ],
+    },
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
   win.loadFile('html/index.html');
 }
 
@@ -67,4 +90,46 @@ async function loadArticles(window) {
     } catch (error) {
     console.error('Error fetching contacts:', error);
     }
+}
+
+async function exportToExcel() {
+  const articles = await database.fetchArticles();
+
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet('Articles');
+
+  const headers = ['RG-Nr','Datum', 'ArtikelNr', 'Beschreibung','Listenpreis', 'Nettopreis', 'Anzahl', 'Einheit'  ];
+  worksheet.addRow(headers);
+
+  // Populate data from Sequelize into the worksheet
+  articles.forEach((article) => {
+    worksheet.addRow([
+      article.transaction_number,
+      article.date,
+      article.articleNr,
+      article.description,
+      article.list_price,
+      article.netto_price,
+      article.quantity,
+      article.unit,
+    ]);
+  });
+
+  const outputPath = dialog.showSaveDialogSync({
+    title: 'Speichere Excel Datei',
+    defaultPath: 'bo artikel.xlsx',
+    filters: [
+      { name: 'Excel Files', extensions: ['xlsx'] }
+    ]
+  });
+
+  if (outputPath) {
+    workbook.xlsx.writeFile(outputPath)
+      .then(() => {
+        console.log('Excel file saved successfully.');
+      })
+      .catch(error => {
+        console.error('Error saving Excel file:', error);
+      });
+  }
 }
